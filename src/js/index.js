51eb02d24312da71425009b9c07607a685d48e5c
kbu715 <kbu715@gmail.com>
2023-04-18 00:10:21 +0900

    feat : 메뉴 삭제하기 and 코드 정리

--- src/js/index.js
+++ src/js/index.js
@@ -7,21 +7,6 @@ function App() {
     e.preventDefault();
   });
 
-  // # 이벤트 위임을 통해 메뉴 수정하기
-  $("#espresso-menu-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("menu-edit-button")) {
-      // https://developer.mozilla.org/ko/docs/Web/API/Element/closest
-      const $menuName = e.target.closest("li").querySelector(".menu-name");
-      const updatedMenuName = prompt(
-        "메뉴명을 수정하세요",
-        $menuName.innerText
-      );
-      if (updatedMenuName) {
-        $menuName.innerText = updatedMenuName;
-      }
-    }
-  });
-
   const addMenu = () => {
     if ($("#espresso-menu-name").value === "") {
       alert("값을 입력해주세요!");
@@ -53,16 +38,47 @@ function App() {
     );
 
     // 총 메뉴 갯수 count
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-    $(".menu-count").innerText = `총 ${menuCount}개`;
+    updateMenuCount();
 
     // input 빈값으로
     $("#espresso-menu-name").value = ``;
   };
-  // 확인버튼 클릭 이벤트 처리
+
+  const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+  };
+
+  // # 이벤트 위임을 통해 메뉴 수정하기
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      // https://developer.mozilla.org/ko/docs/Web/API/Element/closest
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText
+      );
+      if (updatedMenuName) {
+        $menuName.innerText = updatedMenuName;
+      }
+    }
+
+    // # 이벤트 위임을 통해 메뉴 삭제하기
+    if (e.target.classList.contains("menu-remove-button")) {
+      if (confirm("정말 삭제하시겠습니까?")) {
+        const element = e.target.closest("li");
+        element.remove(); // DOM API
+        updateMenuCount();
+      }
+    }
+  });
+
+  // # 클릭이벤트 활용하여 메뉴 추가하기
   $("#espresso-menu-submit-button").addEventListener("click", (e) => {
     addMenu();
   });
+
+  // # 엔터눌러서 메뉴 추가하기
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       return;
